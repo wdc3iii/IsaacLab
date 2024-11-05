1c9423fee1f7c13f19228eb2306fd8e0bce508a2
Will Compton <wdc3iii@gmail.com>
2024-11-05 11:01:48 -0800

    Setup for Raibert Velocity Tracking Controllers on the hopper, g1, go2

---
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/controllers/velocity_raibert_heuristic/__init__.py
@@ -0,0 +1,1 @@
+# Don't need to include any of these packages by default?
---
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/controllers/velocity_raibert_heuristic/hopper.py
@@ -0,0 +1,123 @@
+import torch
+
+
+class VelocityRHHopperCfg:
+    """Configuration for the Velocity Tracking Raibert Heuristic Controller on the Hopper"""
+    # Raibert gains
+    Kp: float = 1.
+    Kd: float = 1.
+    Kff: float = 1.
+    # Clipping Values
+    clip_pos: float = 0.5
+    clip_vel: float = 1
+    clip_ang: float = 0.5
+    clip_ff: float = 0.2
+    # Command maximums for tracking
+    v_max: float = 0.2
+    yaw_rate_max: float = 1.
+    # z height at impact (upright) for approximating time to impact
+    z_impact: float = 0.2
+    # Gravitational constant
+    g: float = 9.81
+
+class VelocityRHHopper:
+
+    def __init__(self, cfg: VelocityRHHopperCfg, num_robots: int, device: str):
+        # store inputs
+        self.cfg = cfg
+        self.num_robots = num_robots
+        self._device = device
+
+        # create buffers
+        # -- commands
+        self.v_des = torch.zeros(num_robots, 2, device=device)
+        self.yaw_rate_des = torch.zeros(num_robots, 1, device=device)
+        # -- gains
+
+    @property
+    def num_actions(self) -> int:
+        # Action is a (v_x, v_y, yaw rate) tuple
+        return 3
+
+    def initialize(self):
+        """Initialize the internals"""
+        pass
+
+    def reset_idx(self, robot_ids: torch.Tensor=None):
+        """Reset the internals"""
+        pass
+
+    def set_command(self, command: torch.Tensor):
+        """Set the target velocity command.
+
+        Args:
+            command: The commmand to set. This is a tensor of shape (num_robots, 3) where
+            the actions are x and y velocities and yaw rate."""
+
+        self.v_des = torch.clip(command[:, :2], -self.cfg.v_max, self.cfg.v_max)
+        self.yaw_rate_des = torch.clip(command[:, 2], -self.cfg.yaw_rate_max, self.cfg.yaw_rate_max)
+
+    def compute(self, v: torch.Tensor, w: torch.Tensor, z: torch.Tensor, q: torch.Tensor):
+        """Computes the desired impact quaternion to track the desired velocity command.
+
+        Args:
+            v: The current center of mass velocity in shape (N, 3).
+            w: The current body angular velocity in shape (N, 3).
+            z: The current center of mass height in shape (N, 1).
+            q: body orientation quaternion in shape (N, 4)."""
+        # Extract variables
+        vz = v[:, 3]
+
+        # Compute impact time
+        t_impact = (vz + torch.sqrt(torch.square(vz) + 2 * self.cfg.g * torch.clamp(z - self.cfg.z_impact, min=0))) / self.cfg.g
+
+        # Compute the desired impact position (relative to current position).
+        p_des = self.v_des * t_impact
+        yaw_des = self.yaw_rate_des * t_impact
+
+        # Predict the next impact location (relative to current position).
+        p_pred = v[:, :2] * t_impact
+        yaw_pred = w[:, 3] * t_impact
+
+        # Implement Raibert Heuristic on these positions
+        quat_des = self._compute_raibert_heuristic(p_des - p_pred, yaw_des - yaw_pred, v, w, q)
+        return quat_des
+
+    def _compute_raibert_heuristic(self, delta_p, delta_yaw, v, w, q):
+        pitch_d = torch.clip(
+            - self.cfg.Kp * torch.clip(-delta_p[:, 0], -self.cfg.clip_pos, self.cfg.clip_pos) \
+            - self.cfg.Kd * torch.clip(v[:, 0], -self.cfg.clip_vel, self.cfg.clip_vel) \
+            + self.cfg.Kff * torch.clip(-self.v_des[:, 0], -self.cfg.clip_ff, self.cfg.clip_ff),
+            -self.cfg.clip_ang, self.cfg.clip_ang
+        )
+        roll_d = torch.clip(
+            - self.cfg.Kp * torch.clip(delta_p[:, 1], -self.cfg.clip_pos, self.cfg.clip_pos) \
+            - self.cfg.Kd * torch.clip(-v[:, 1], -self.cfg.clip_vel, self.cfg.clip_vel) \
+            + self.cfg.Kff * torch.clip(self.v_des[:, 0], -self.cfg.clip_ff, self.cfg.clip_ff),
+            -self.cfg.clip_ang, self.cfg.clip_ang
+        )
+        yaw_d = torch.clip(delta_yaw, -self.cfg.clip_ang, self.cfg.clip_ang) + self._quat2yaw(q)
+        return self._rpy2quat(roll_d, pitch_d, yaw_d)
+
+    @staticmethod
+    def _quat2yaw(q: torch.Tensor) -> torch.Tensor:
+        w, x, y, z = q[..., 0], q[..., 1], q[..., 2], q[..., 3]
+        siny_cosp = 2.0 * (w * z + x * y)
+        cosy_cosp = 1.0 - 2.0 * (y * y + z * z)
+        yaw = torch.atan2(siny_cosp, cosy_cosp)
+        return yaw
+
+    @staticmethod
+    def _rpy2quat(r: torch.Tensor, p: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        cy = torch.cos(y * 0.5)
+        sy = torch.sin(y * 0.5)
+        cp = torch.cos(p * 0.5)
+        sp = torch.sin(p * 0.5)
+        cr = torch.cos(r * 0.5)
+        sr = torch.sin(r * 0.5)
+
+        w = cr * cp * cy + sr * sp * sy
+        x = sr * cp * cy - cr * sp * sy
+        y = cr * sp * cy + sr * cp * sy
+        z = cr * cp * sy - sr * sp * cy
+        return torch.stack((w, x, y, z), dim=-1)
